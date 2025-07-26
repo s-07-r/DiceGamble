1454c28d234bb1886df1e367dc4f1bfccfd8f641
unknown <sharanya.rakeshi@gmail.com>
2025-07-26 22:35:04 +0530

    feat: initial project setup with main files

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+class Dice
+{
+    static void Main( string[] args)
+    {
+        Console.WriteLine("Welcome to the game :D\n");
+        Console.WriteLine("Please roll the die !\n");
+        Console.WriteLine("press Enter to roll the die.\n");
+        Console.ReadLine();
+        Random random = new Random();
+        int roll = random.Next(1, 7);
+        int count = 1;
+        while (roll != 6)
+        {
+          
+            do
+            {
+                Console.WriteLine("Not quite there yet! Try again. You rolled, {0}.", roll);
+                Console.WriteLine("LET'S GO AGAIN.  ");
+                Console.ReadLine();
+                roll = random.Next(1, 7);
+                count++;
+
+            } while (roll != 6);
+        }
+        Console.WriteLine("You rolled the die {0} times before you got a 6, Loser.", count);
+
+    }
+}
